d4d5cfc7ce49486584db5b61d38a11a164bf5973
sandipan45 <sandipan45@gmail.com>
2020-06-11 17:02:27 +0530

    device dection system added

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -11,7 +11,7 @@ const $ = require("jquery")
 
 export const onInitialClientRender = () => {
   $.getJSON('https://ipapi.co/json/', function(data){
-	console.log("browser.render data", data);
+	//console.log("browser.render data", data);
     $("#ipaddress").html(data.ip);
 	$("#country").html(data.country_name);
 	$("#city").html(data.city);
@@ -20,4 +20,35 @@ export const onInitialClientRender = () => {
 	$("#timezone").html(data.timezone);
   });
   
+	$(document).ready(function () {
+		var instance = $.fn.deviceDetector;
+		console.log(instance.getInfo());
+		$("#isAandroid").html(instance.getInfo().android);
+		$("#isBlackberry").html(instance.getInfo().blackberry);
+		$("#isDesktop").html(instance.getInfo().desktop);
+		$("#browserName").html(instance.getInfo().browserName);
+		$("#browserVersion").html(instance.getInfo().browserVersion);
+		$("#isBsd").html(instance.getInfo().bsd);
+		$("#isChrome").html(instance.getInfo().chrome);
+		$("#isEdge").html(instance.getInfo().edge);
+		$("#isFirefox").html(instance.getInfo().firefox);
+		$("#isIe").html(instance.getInfo().ie);
+		$("#isieMobile").html(instance.getInfo().ieMobile);
+		$("#isIos").html(instance.getInfo().ios);
+		$("#isIpad").html(instance.getInfo().ipad);
+		$("#isIphone").html(instance.getInfo().iphone);
+		$("#isLinux").html(instance.getInfo().linux);
+		$("#isMacos").html(instance.getInfo().macos);
+		$("#isMobile").html(instance.getInfo().mobile);
+		$("#isMsie").html(instance.getInfo().msie);
+		$("#isOpera").html(instance.getInfo().opera);
+		$("#isOperaMini").html(instance.getInfo().operaMini);
+		$("#isOsId").html(instance.getInfo().osId);
+		$("#isOsname").html(instance.getInfo().osName);
+		$("#isOsVersion").html(instance.getInfo().osVersion);
+		$("#isosVersionBugfix").html(instance.getInfo().osVersionBugfix);
+		$("#isSafari").html(instance.getInfo().safari);
+		$("#isWindows").html(instance.getInfo().windows);
+		$("#isWindowsPhone").html(instance.getInfo().windowsPhone);
+	});
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,15 +6,46 @@ import Image from "../components/image"
 import SEO from "../components/seo"
 import App from "../components/ipaddress"
 
+import "./dist/jquery.device.detector.js"
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <div>Ip Address: <span id="ipaddress"></span></div>
-	<div>Country: <span id="country"></span></div>
-	<div>City: <span id="city"></span></div>
-	<div>Zip/Postal Code: <span id="zip"></span></div>
-	<div>ISP: <span id="isp"></span></div>
-	<div>Timezone: <span id="timezone"></span></div>
+    <div>Ip Address: <span id="ipaddress" className="valueRed"></span></div>
+	<div>Country: <span className="valueRed" id="country"></span></div>
+	<div>City: <span className="valueRed" id="city"></span></div>
+	<div>Zip/Postal Code: <span className="valueRed" id="zip"></span></div>
+	<div>ISP: <span className="valueRed" id="isp"></span></div>
+	<div>Timezone: <span className="valueRed" id="timezone"></span></div>
+	<p>&nbsp;</p>
+	<h4>Device Track::</h4>
+	<div>Android: <span className="valueRed" id="isAandroid"></span></div>
+	<div>Blackberry: <span className="valueRed" id="isBlackberry"></span></div>
+	<div>Desktop: <span className="valueRed" id="isDesktop"></span></div>
+	<div>Browser Name: <span className="valueRed" id="browserName"></span></div>
+	<div>Browser Version: <span className="valueRed" id="browserVersion"></span></div>
+	<div>BSD: <span className="valueRed" id="isBsd"></span></div>
+	<div>Chrome: <span className="valueRed" id="isChrome"></span></div>
+	<div>Edge: <span className="valueRed" id="isEdge"></span></div>
+	<div>Firefox: <span className="valueRed" id="isFirefox"></span></div>
+	<div>IE: <span className="valueRed" id="isIe"></span></div>
+	<div>IE Mobile: <span className="valueRed" id="isieMobile"></span></div>
+	<div>IOS: <span className="valueRed" id="isIos"></span></div>
+	<div>Ipad: <span className="valueRed" id="isIpad"></span></div>
+	<div>Iphone: <span className="valueRed" id="isIphone"></span></div>
+	<div>Linux: <span className="valueRed" id="isLinux"></span></div>
+	<div>Mac Os: <span className="valueRed" id="isMacos"></span></div>
+	<div>Mobile: <span className="valueRed" id="isMobile"></span></div>
+	<div>Msie: <span className="valueRed" id="isMsie"></span></div>
+	<div>Opera: <span className="valueRed" id="isOpera"></span></div>
+	<div>Opera Mini: <span className="valueRed" id="isOperaMini"></span></div>
+	<div>OsId: <span className="valueRed" id="isOsId"></span></div>
+	<div>Os Name: <span className="valueRed" id="isOsname"></span></div>
+	<div>Os Version: <span className="valueRed" id="isOsVersion"></span></div>
+	<div>Os Version Bugfix: <span className="valueRed" id="isosVersionBugfix"></span></div>
+	<div>Safari: <span className="valueRed" id="isSafari"></span></div>
+	<div>Windows: <span className="valueRed" id="isWindows"></span></div>
+	<div>WindowsPhone: <span className="valueRed" id="isWindowsPhone"></span></div>
   </Layout>
 )
 
